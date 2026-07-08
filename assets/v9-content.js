e8fa323bea2b1fcab4bae06e27ea23af842c020c
Imran Ali Namazi <imran@amadeusweb.world>
2026-07-09 03:59:41 +0530

    prompt and permalink

--- assets/v9-content.js
+++ assets/v9-content.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
 		this.style.height = '1px';
 		this.style.height = this.scrollHeight + 'px';
 	}
+
+	if ($('body.has-gemini-ai').length) {
+		let numbers = $('a.prompt-number');
+		if (!numbers.length) {
+			const prompts = $('.prompt');
+			prompts.each(function(ix, el){
+				ix += 1;
+				$(el).prepend($('<a name="prompt-' + ix + '" class="prompt-number"></a>'));
+			});
+			numbers = $('a.prompt-number');
+		}
+
+		if (numbers.length) {
+			numbers.each(function(ix, el) {
+				ix = $(el).attr('name').replace('prompt-', '');
+				$(el).closest('.prompt').prepend($('<div class="prompt-button">Prompt <a href="#prompt-' + ix + '">#' + ix + '</a></div>'));
+			});
+		}
+	}
 });
