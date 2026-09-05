0490d113316696cacdacf681f308746adee610cf
Imran Ali Namazi <imran@joyfulearth.org>
2026-09-05 13:42:53 +0530

    aztras and panels

--- builder/network/panels.php
+++ builder/network/panels.php
@@ -1,15 +1,15 @@
 <?php
 variable('network-panels', [
 	'divFormat' => '<div class="p-3 col-md-%s col-sm-12 align">' . NEWLINE,
-	'sectionFormat' => '	<section class="network-site%s text-center%s">' . NEWLINE,
+	'sectionFormat' => '	<section class="network-site%s my-3 text-center%s">' . NEWLINE,
 ]);
 
 function renderSides(string $group) {
-	$sheet = getSheet(NETWORKPATH . '/sites.tsv', 'group');
-	$relativePrefix = substr(NETWORKPATH, strlen(ALLSITESROOT)) . '/';
+	$sheet = getSheet(NETWORKPATH . '/data/sites.tsv', 'group');
+	$relativePrefix = domain::$current->folder;
 	foreach ($sheet->group[$group] as $item) {
 		$slug = $sheet->getValue($item, 'slug');
-		if (!disk_is_dir(NETWORKPATH . '/' . $slug)) continue;
+		if (!disk_is_dir(ALLSITESROOT . $relativePrefix . $slug)) continue;
 		$site = getSiteInfo($relativePrefix . $slug);
 		$class = ' site-' . $site->key . '-bgd';
 		echo sprintf(subVariable('network-panels', 'sectionFormat'), $class . ' site-button', '');
@@ -19,7 +19,7 @@ function renderSides(string $group) {
 }
 
 function renderCenter(string $logo) {
-	$site = getSiteInfo(substr(NETWORKPATH, strlen(ALLSITESROOT)) . '/home');
+	$site = getSiteInfo(substr(NETWORKPATH, strlen(ALLSITESROOT)));
 	
 	renderNetworkPanel($site, ['in-center' => true, 'logo' =>  'assets/' . $logo]);
 	echo '<div class="network-splash-name text-center">' . $site->byline . '</div>';
@@ -29,9 +29,10 @@ function renderNetworkPanel(site $site, array $settings) {
 	$result = [];
 
 	$url = $site->getUrl(_getUrlKeySansPreview());
+	$logo = '<img src="' . $url . valueIfSet($settings, 'logo', $site->key . '-logo.png') . '" height="100" class="img-fluid img-max-400 content-network-logo" />';
+	$result[] = '<div class="text-center">' . $logo . '</div>';
+
 	if (valueIfSet($settings, 'in-center')) {
-		$logo = '<img src="' . $url . $settings['logo'] . '" height="100" class="img-fluid img-max-400 content-network-logo" />';
-		$result[] = '<div class="text-center">' . $logo . '</div>';
 		echo implode(NEWLINE, $result);
 		return;
 	}
